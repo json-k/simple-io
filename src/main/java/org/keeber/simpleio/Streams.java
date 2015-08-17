89efc53d90a7f594315d0d1c586af9415849e12a
Jason <jason@keeber.org>
2015-08-17 10:05:46 -0400

    1.0.3

--- src/main/java/org/keeber/simpleio/Streams.java
+++ src/main/java/org/keeber/simpleio/Streams.java
@@ -1,12 +1,21 @@
 package org.keeber.simpleio;
 
 import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
 import java.io.FilterInputStream;
 import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * <p>
+ * Utility methods relating to streams.
+ * 
+ * 
+ * @author Jason
+ *
+ */
 public class Streams {
 
 	public static byte[] asByteArray(InputStream is) throws IOException {
@@ -14,12 +23,13 @@ public class Streams {
 		copy(is, bos, true);
 		return bos.toByteArray();
 	}
- 
+
 	public static String asString(InputStream is) throws IOException {
 		return new String(asByteArray(is));
 	}
 
-	public static void copy(InputStream is, OutputStream os, boolean close) throws IOException {
+	public static void copy(InputStream is, OutputStream os, boolean close)
+			throws IOException {
 		byte[] buffer = new byte[1024 * 8];
 		int len;
 		while ((len = is.read(buffer)) > 0) {
@@ -27,8 +37,32 @@ public class Streams {
 		}
 		os.flush();
 		if (close) {
-			is.close();
-			os.close();
+			Streams.close(is);
+			Streams.close(os);
+		}
+	}
+
+	/**
+	 * <p>
+	 * Closes the provided stream in a null safe way without an exception.
+	 * 
+	 * <p>
+	 * <i>Q: Shouldn't this be called closeQuietly (or something)?
+	 * <p>
+	 * <i>A: There is already a close method, so the implication is that this
+	 * method does something in addition to it (which it does). I figured it was
+	 * more efficient, except for writing this explanation.
+	 * 
+	 * @param stream
+	 *            to be closed (quietly)
+	 */
+	public static void close(Closeable stream) {
+		if (stream != null) {
+			try {
+				stream.close();
+			} catch (IOException e) {
+				// Ignore (that is the only function of this method.
+			}
 		}
 	}
 
@@ -38,7 +72,8 @@ public class Streams {
 
 	}
 
-	public static class CountingOutputStream extends FilterOutputStream implements CountingStream {
+	public static class CountingOutputStream extends FilterOutputStream
+			implements CountingStream {
 		private long count;
 
 		public CountingOutputStream(OutputStream os) {
@@ -49,19 +84,22 @@ public class Streams {
 			return count;
 		}
 
-		@Override public void write(byte[] b, int off, int len) throws IOException {
+		@Override
+		public void write(byte[] b, int off, int len) throws IOException {
 			out.write(b, off, len);
 			count += len;
 		}
 
-		@Override public void write(int b) throws IOException {
+		@Override
+		public void write(int b) throws IOException {
 			out.write(b);
 			count++;
 		}
 
 	}
 
-	public static class CountingInputStream extends FilterInputStream implements CountingStream {
+	public static class CountingInputStream extends FilterInputStream implements
+			CountingStream {
 		private long count;
 		private long mark = -1;
 
@@ -73,7 +111,8 @@ public class Streams {
 			return count;
 		}
 
-		@Override public int read() throws IOException {
+		@Override
+		public int read() throws IOException {
 			int result = in.read();
 			if (result != -1) {
 				count++;
@@ -81,7 +120,8 @@ public class Streams {
 			return result;
 		}
 
-		@Override public int read(byte[] b, int off, int len) throws IOException {
+		@Override
+		public int read(byte[] b, int off, int len) throws IOException {
 			int result = in.read(b, off, len);
 			if (result != -1) {
 				count += result;
@@ -89,18 +129,21 @@ public class Streams {
 			return result;
 		}
 
-		@Override public long skip(long n) throws IOException {
+		@Override
+		public long skip(long n) throws IOException {
 			long result = in.skip(n);
 			count += result;
 			return result;
 		}
 
-		@Override public synchronized void mark(int readlimit) {
+		@Override
+		public synchronized void mark(int readlimit) {
 			in.mark(readlimit);
 			mark = count;
 		}
 
-		@Override public synchronized void reset() throws IOException {
+		@Override
+		public synchronized void reset() throws IOException {
 			if (!in.markSupported()) {
 				throw new IOException("Mark not supported");
 			}
